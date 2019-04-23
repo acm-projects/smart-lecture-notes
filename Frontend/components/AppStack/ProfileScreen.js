51704dccb366738bd85202fbd99ffb3a88cddcc0
Jacob-aa-garcia <47547652+Jacob-aa-garcia@users.noreply.github.com>
2019-04-22 22:25:50 -0500

    Update ProfileScreen.js

--- Frontend/components/AppStack/ProfileScreen.js
+++ Frontend/components/AppStack/ProfileScreen.js
@@ -1,100 +1,89 @@
 import React, { Component } from 'react';
-import {
-	StyleSheet,
-	Text,
-	View, Image,
-	ScrollView,
-	TouchableOpacity,
-	SegmentedControlIOS,
-	SafeAreaView,
-	Button
-} from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
-import { Ionicons } from '@expo/vector-icons';
+//import { Ionicons } from '@expo/vector-icons';
 
 // import OrderHistory from './OrderHistory'
 // import Schedule from './Schedule'
 
-class ClassesScreen extends Component {
-
-	static navigationOptions = ({ navigation }) => {
-		return {
-			title: 'Profile',
-			headerRight: (
-				<TouchableOpacity
-					style={{ paddingRight: 10 }}
-					onPress={() => {
-						navigation.navigate('Settings')
-					}}
-				>
-					<Ionicons name="ios-options" color='#007AFF' size={28} />
-				</TouchableOpacity>
-			)
-		}
-	}
-
-	state = {
-		selectedIndex: 0
-	}
-
+class ProfileScreen extends Component {
+	static navigationOptions = {
+        header: null
+    }; 
 	render() {
-
-		const { navigate } = this.props.navigation;
-
 		return (
 			<View style={styles.container}>
-				<ScrollView
-					showsVerticalScrollIndicator={false}
-				>
-
-					{/* !Post Call */}
-
-					<View style={styles.titleBar}>
-						<Image style={styles.avatar} source={require('../../assets/temoc.jpg')} />
-						<Text style={styles.name}>Temoc</Text>
-						<Text style={styles.title}>@comet</Text>
+				<View style={styles.titleBar}>
+					<Image
+						style={styles.avatar}
+						source={require('../../assets/huy.jpg')}
+					/>
+					<Text style={styles.name}>Huy Pham</Text>
+				</View>
+				<View style={styles.container}>
+					<View style={{ width: '100%',
+									justifyContent: 'center',
+									backgroundColor: '#ffcc5c',
+									height: 40,
+									flex: 1,
+									padding: 30,
+									marginTop: 20,
+									alignItems: 'center' }}
+					>
+						<Text style={styles.setting}>Edit Profile</Text>
 					</View>
-					<View style={{ marginTop: 30 }}>
-						<TouchableOpacity
-							style={styles.button}
-							onPress={() => navigate('Login')}>
-							<Text style={styles.buttonText}>Log Out</Text>
-						</TouchableOpacity>
+					<View style={{ width: '100%',
+									justifyContent: 'center',
+									backgroundColor: '#ffb3b3',
+									height: 40,
+									flex: 1,
+									padding: 30,
+									
+									alignItems: 'center' }}
+					>
+						<Text style={styles.setting}>Export Notes</Text>
 					</View>
-
-					{/* <Text style={styles.subTitle}>
-                         Your classes
-                         </Text> */}
-
-					{/* <SafeAreaView>
-						<SegmentedControlIOS
-							style={styles.segmentedIOS}
-							values={['Schedule', 'Order\'s History']}
-							selectedIndex={this.state.selectedIndex}
-							onChange={(event) => {
-								this.setState({ selectedIndex: event.nativeEvent.selectedSegmentIndex });
-							}}
-						/>
-						<View>
-							{(this.state.selectedIndex === 0) ? (
-								<Schedule />
-							) : (
-								<OrderHistory />
-							)}
-						</View>
-					</SafeAreaView> */}
-				</ScrollView>
-			</View>
+					<View style={{ width: '100%',
+									justifyContent: 'center',
+									backgroundColor: '#98bdf0',
+									height: 40,
+									flex: 1,
+									padding: 30,
+									alignItems: 'center' }}
+					>
+						<Text style={styles.setting}>Settings</Text>
+					</View>
+					<View style={{ width: '100%',
+									justifyContent: 'center',
+									backgroundColor: 'white',
+									height: 80,
+									flex: 1,
+									padding: 40,
+									alignItems: 'center' }}
+					/>
+					<View style={{ width: '100%',
+									justifyContent: 'center',
+									backgroundColor: '#ff6f69',
+									height: 40,
+									flex: 1,
+									padding: 30,									
+									alignItems: 'center' }}
+					>
+						<Text style={styles.setting}>Log Out</Text>
+					</View>
+				</View>
+			</View>	
 		);
 	}
 }
 
-export default ClassesScreen;
+export default ProfileScreen;
 
 const styles = StyleSheet.create({
 	container: {
-		flex: 1,
-		backgroundColor: 'rgb(255, 255, 255)'
+		flexDirection: 'column',
+		backgroundColor: 'rgb(255, 255, 255)',
+		justifyContent: 'center'
 	},
 	segmentedIOS: {
 		marginTop: 10,
@@ -109,7 +98,7 @@ const styles = StyleSheet.create({
 	},
 	titleBar: {
 		width: '100%',
-		marginTop: 15,
+		marginTop: 70,
 		justifyContent: 'center',
 		alignItems: 'center',
 	},
@@ -144,22 +133,10 @@ const styles = StyleSheet.create({
 		fontFamily: 'Avenir Next',
 		fontSize: 20,
 	},
-	button: {
-		backgroundColor: '#E8384F',
-		marginLeft: 5,
-		marginRight: 5,
-		borderRadius: 20,
-		marginBottom: 10,
-		paddingHorizontal: 20,
-		height: 40,
-		alignItems: 'center',
-		justifyContent: 'center'
-	},
-	buttonText: {
-		textAlign: 'center',
-		color: 'white',
-		fontSize: 20,
+	setting: {
+		fontSize: 16,
 		fontFamily: 'Avenir Next',
-		fontWeight: '500'
+		color: 'white',
+		fontWeight: '600',
 	}
 });
